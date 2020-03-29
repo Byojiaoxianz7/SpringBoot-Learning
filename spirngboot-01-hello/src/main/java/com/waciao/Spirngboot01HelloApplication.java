94380f003e12d641f7baffa41d1eacdcf6809334
byojiaoxianz7 <byaojiaoxianz7@outlook.com>
2020-03-29 16:57:11 +0800

    Create SpringBoot-Learning

---
+++ spirngboot-01-hello/src/main/java/com/waciao/Spirngboot01HelloApplication.java
@@ -0,0 +1,13 @@
+package com.waciao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spirngboot01HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spirngboot01HelloApplication.class, args);
+    }
+
+}
---
+++ spirngboot-01-hello/src/test/java/com/waciao/Spirngboot01HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waciao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spirngboot01HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
